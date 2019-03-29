1461872bbe5446cea04d317c0790019392dad3c8
HerryPO <1184897679@qq.com>
2019-03-30 01:15:57 +0800

    the 3rd

--- basepart.py
+++ basepart.py
@@ -4,7 +4,7 @@ Created on Wed Mar 27 22:26:06 2019
 
 @author: lenovo
 """
-
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,7 +12,7 @@ class datalist:
     def __init__(self,name,fraction,kind):
         self.name=name
         self.fraction=fraction
-        #self.kind=kind
+        self.kind=kind
         
 
 def getsubject(txt):
@@ -22,16 +22,31 @@ def getsubject(txt):
     r.encoding=r.apparent_encoding
     return r.text
 
+def getpic(j,picp):
+    root="D://doubanpics//"
+    path=root + str(j)+".jpg"
+    if not os.path.exists(root):
+        os.mkdir(root)
+    p=requests.get(picp)
+    with open(path,'wb') as f:
+        f.write(p.content)
+
 def dealurl(html):
     soup = BeautifulSoup(html,"html.parser")
-    contentlist = soup.find_all("div","content")
+    contentlist = soup.find_all("div","result")
     ndatalist=[]
-    for i in contentlist[0:5]:
+    for j in range(0,5):
+        i=contentlist[j]
+        k=i.find("div",class_="title")
+        l=i.find("img")
+        picp=l.attrs['src']
+        getpic(j,picp)
         try:
             j=i.find("span","rating_nums")
-            x=datalist(i.div.a.string,j.string)
+            x=datalist(k.a.string,j.string,k.span.string)
             ndatalist.append(x)
         except:
-            x=datalist(i.div.a.string,'尚未上映')
+            x=datalist(k.a.string,'尚未上映',k.span.string)
             ndatalist.append(x)
+        
     return ndatalist
--- mainpart.py
+++ mainpart.py
@@ -4,31 +4,40 @@ Created on Thu Mar 28 15:23:53 2019
 
 @author: lenovo
 """
-
+import os
 from basepart import *
 import tkinter as tk
-
+from PIL import Image
+from PIL import ImageTk
 import requests
 from bs4 import BeautifulSoup
 
 window=tk.Tk()
 window.title('search-douban')
-window.geometry('500x300')
+window.geometry('600x500')
 
 e=tk.Entry(window,show=None)
 e.place(x=100,y=20)
 
 var1=tk.StringVar()
 var2=tk.StringVar()
-
+var3=tk.StringVar()
 t=[]
 i=0
+
+def setpic(j):
+    global imagen
+    file = Image.open("D://doubanpics//"+str(j)+".jpg")
+    imagen=ImageTk.PhotoImage(file)
+    k=tk.Label(window,image=imagen)    
+    k.place(x=50,y=80)
 
 def search():
     f=e.get()
     if(f is None):
         var1.set('')
         var2.set('')
+        var3.set('')
     else:
         html=getsubject(f)
         global ndatalist
@@ -37,6 +46,8 @@ def search():
         i=0
         var1.set(ndatalist[0].name)
         var2.set(ndatalist[0].fraction)
+        var3.set(ndatalist[0].kind)
+        setpic(0)
 
 def gonext():
     try:
@@ -44,9 +55,12 @@ def gonext():
         i=(i+1)%5
         var1.set(ndatalist[i].name)
         var2.set(ndatalist[i].fraction)
+        var3.set(ndatalist[i].kind)
+        setpic(i)
     except:
         var1.set('')
         var2.set('')
+        var3.set('')
 
 def golast():
     try:
@@ -57,25 +71,32 @@ def golast():
             i=i-1
         var1.set(ndatalist[i].name)
         var2.set(ndatalist[i].fraction)
+        var3.set(ndatalist[i].kind)
+        setpic(i)
     except:
         var1.set('')
         var2.set('')
+        var3.set('')
 
 a1=tk.Label(window,textvariable=var1,width=30,height=1)
-a1.place(x=250,y=100)
+a1.place(x=400,y=150)
 a2=tk.Label(window,textvariable=var2,width=30,height=1)
-a2.place(x=250,y=120)
+a2.place(x=400,y=170)
+a3=tk.Label(window,textvariable=var3,width=30,height=1)
+a3.place(x=400,y=190)
 f1=tk.Label(window,text='Key World:',width=10,height=1)
 f1.place(x=20,y=18)
 f2=tk.Label(window,text='   Name:',width=9,height=1)
-f2.place(x=230,y=100)
+f2.place(x=380,y=150)
 f3=tk.Label(window,text='Farction:',width=9,height=1)
-f3.place(x=230,y=120)
+f3.place(x=380,y=170)
+f4=tk.Label(window,text='     Kind:',width=9,height=1)
+f4.place(x=380,y=190)
 b=tk.Button(window,text='search',width=7,height=1,command=search)
 b.place(x=250,y=15)
 b1=tk.Button(window,text='last',width=7,height=1,command=golast)
-b1.place(x=320,y=250)
+b1.place(x=420,y=350)
 b2=tk.Button(window,text='next',width=7,height=1,command=gonext)
-b2.place(x=400,y=250)
+b2.place(x=500,y=350)
 
 window.mainloop()
